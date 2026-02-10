bc677b754d128916e3e04c9eabadf8722cf395e4
’melekbouanen’ <mohamedmalek.bouanen@esprit.tn>
2026-02-10 19:35:33 +0100

    Sync files from employe1 branch

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,56 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
+#include <QGraphicsDropShadowEffect>
+#include <QPropertyAnimation>
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QGraphicsDropShadowEffect *glow = new QGraphicsDropShadowEffect(this);
+    glow->setBlurRadius(0);
+    glow->setOffset(0, 0);
+    glow->setColor(QColor(0, 170, 255, 180)); // bleu lumineux
+
+    ui->frame_43->setGraphicsEffect(glow);
+    QPropertyAnimation *anim = new QPropertyAnimation(glow, "blurRadius");
+
+    anim->setStartValue(0);
+    anim->setEndValue(40);
+    anim->setEasingCurve(QEasingCurve::OutCubic);
+    anim->start();
+    ui->stackedWidget->setCurrentIndex(0);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+
+
+
+
+void MainWindow::on_cnxemp_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(2);
+}
+
+
+void MainWindow::on_dec_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(0);
+}
+
+
+void MainWindow::on_mdp_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(1);
+}
+
+
+void MainWindow::on_retour_2_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(0);
+}
+
