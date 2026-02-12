043dc8494c9b81d99879d4577b9e5c30fb30cdc5
melekbouanen <mohamedmalek.bouanen@esprit.tn>
2026-02-12 01:51:09 +0100

    amélioration de l'interface de gestion des clients

--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,18 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QTableWidget>
+#include <QLineEdit>
+#include <QComboBox>
+#include <QMessageBox>
+#include <QFileDialog>
+#include <QTextStream>
+#include <QDialog>
+#include <QVBoxLayout>
+#include <QLabel>
+#include <QFrame>
+#include <QPushButton>
+#include <QGridLayout>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -18,31 +30,54 @@ public:
     ~MainWindow();
 
 private slots:
-    void on_pushButton_clicked();
+    // Slot pour le bouton Rechercher (utilise lineEdit_8)
+    void on_pushButton_21_clicked();
 
+    // Slot pour le bouton Exporter
     void on_pushButton_20_clicked();
 
-    void on_pushButton_29_clicked();
+    // Slot pour le bouton Statistique
+    void on_pushButton_19_clicked();
 
-    void on_pushButton_17_clicked();
+    // Slot pour le bouton Modifier
+    void on_pushButton_23_clicked();
 
-    void on_pushButton_30_clicked();
+    // Slot pour le bouton Supprimer
+    void on_pushButton_10_clicked();
 
-    void on_pushButton_31_clicked();
+    // Slot pour le bouton Ajouter (dans frame_48)
+    void on_pushButton_client_ajouter_clicked();
 
-    void on_cnx_clicked();
-
-    void on_cnx1_clicked();
-
-    void on_cnxemp_clicked();
-
-    void on_dec_clicked();
-
-    void on_mdp_clicked();
-
-    void on_retour_2_clicked();
+    // Slot pour le combobox de tri
+    void on_comboTypePropriete_3_currentIndexChanged(int index);
 
 private:
     Ui::MainWindow *ui;
+
+    // Méthodes privées pour la gestion
+    void initialiserInterface();
+    void configurerTableau();
+    void chargerDonneesExemples();
+    void afficherWidgetAjout();        // Popup d'ajout
+    void afficherWidgetModification();
+    void afficherWidgetStatistiques();
+    void trierTableau(int critere);
+    void rechercherClient();
+    void exporterVersPDF();
+    void supprimerClient();
+
+    // Variables pour stocker les données temporaires
+    struct ClientData {
+        int id;
+        QString nom;
+        QString prenom;
+        QString type;
+        QString zone;
+        QString telephone;
+        QString statut;
+        QString cin;
+    };
+
+    QList<ClientData> clientsData;
 };
 #endif // MAINWINDOW_H
